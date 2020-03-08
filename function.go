eeac3554989f2a7fe3812a66a70c4c0803716faa
mt-village <mt.village.ken@gmail.com>
2020-03-08 18:20:02 +0900

    example for pub/sub function

---
+++ function.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+)
+
+// User JSONデコード用の構造体.
+type User struct {
+	Name string `json:"name"`
+}
+
+// HelloPubSub Pub/Subからトリガーされる関数.
+func HelloPubSub(ctx context.Context, m *pubsub.Message) error {
+
+	// メッセージの出力
+	var user User
+	if err := json.Unmarshal(m.Data, &user); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Hello,", user.Name)
+
+	// 属性の出力
+	if len(m.Attributes) > 0 {
+		for k, v := range m.Attributes {
+			log.Println("key:", k)
+			log.Println("value:", v)
+		}
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mt-village/cloud-func-example
+
+go 1.11
+
+require cloud.google.com/go/pubsub v1.3.0
